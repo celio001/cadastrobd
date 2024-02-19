0c1b851d4a76515a45a1da5abf15cb2d6cd3b70d
celio001 <pessoalcelio01@gmail.com>
2024-02-19 18:00:37 -0300

    Cadastro de usuario

--- conexaobd.py
+++ conexaobd.py
@@ -5,18 +5,30 @@ class ConsultaUsuarios:
         self.conexao = sqlite3.connect('usuarios.db')
         self.sql = self.conexao.cursor()
     
+    #Extraindo e mostrando os usuarios da tabela
     def Usuariosdb(self):
         self.usuarios =  self.sql.execute('select * from usuario;')
 
         for self.usuario in self.usuarios:
-            nome, email, senha, telefone, cargo = self.usuario
+            nome, email, senha, cpf, cargo = self.usuario
             print('\n')
             print(f"Nome: {nome}")
             print(f"Email: {email}")
             print(f"Senha: ******")
-            print(f"CPF: {******}")
-            print(f"Cargo: {cargo}") 
+            print(f"CPF: *****")
+            print(f"Cargo: {cargo}")
+        self.conexao.close()
+    
+    #Adicionando usuario a nossa tabela de usuarios
+    def cadastrarUsuario(self, nome: str, email: str, senha: str, cpf: int, cargo: str):
+        self.cadastrar = []
+        self.cadastrar.extend([nome, email, senha,cpf, cargo])
+
+        self.sql.execute('insert into usuario values(?,?,?,?,?)', self.cadastrar)
+    
+        self.conexao.commit()
+        self.conexao.close()
 
 if __name__ == '__main__':
     iniciar = ConsultaUsuarios()
-    iniciar.Usuariosdb()
+    iniciar.cadastrarUsuario()
--- main.py
+++ main.py
@@ -1,23 +1,41 @@
 from conexaobd import ConsultaUsuarios
+import os
 
 class Menu():
     def __init__(self):
-        pass
+        os.system('cls')
 
-    def menu(self):
+    def menu1(self):
         print('Bem-vindo ao sistema UserRegistry \n')
-        #self.opcaomn = input('Digite o número do que deseja fazer: \n (1)logar\n (2)Consultar Usuarios\n (3)Cadastrar usuario\n')
-        #self.login()
-        self.exibirusUsuarios()
-    
+        self.opcaomn1 = int(input('Digite o número do que deseja fazer: \n (1)logar\n (2)Sair\n'))
+        while self.opcaomn1 <= 0 or self.opcaomn1 >= 3:
+            print('!! Digitar uma opção valida !!')
+            self.opcaomn1 = input('Digite o número do que deseja fazer: \n (1)logar\n (2)Sair\n')
+
+        if self.opcaomn1 == 1:
+            self.login()
+        elif self.opcaomn1 == 2:
+            exit()
+
     def login(self):
         print('Bem-vindo a area de registro!')
-        email = input('Inserir e-mail:')
-        senha = input('Inserir senha:')
+        self.email = input('Inserir e-mail:')
+        self.senha = input('Inserir senha:')
     
     def exibirusUsuarios(self):
         consulta = ConsultaUsuarios()
         return consulta.Usuariosdb()
+    
+    def cadastrarUsuarios(self):
+        print('[ Area de cadastro de usuarios ]')
+        self.nome = input('Nome: ')
+        self.email = input('E-mail: ')
+        self.senha = input('Senha: ')
+        self.cpf = input('CPF: ')
+        self.cargo = input('Cargo: ')
+        
+        self.cadastrar = ConsultaUsuarios
+        self.cadastrar.cadastrarUsuario(self.nome,self.email,self.senha,self.cpf, self.cargo)
 
 iniciar = Menu()
-iniciar.exibirusUsuarios()
+iniciar.cadastrarUsuarios()
